71c410a3a29ddd98453d94cfa225ae456fed7bd5
HyKo <happy.plan7983@rt.hyko.dev>
2024-11-05 17:10:21 -0500

    Create acary-cloud-helper.php

---
+++ acary-cloud-helper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:   Cloud - Helper
+ * Description:   Ce plugin, conçu par Acary, est un plugin multifonction conçu pour les sites WordPress hébergés sur nos serveurs cloud, offrant une gamme d'outils pratiques pour simplifier la gestion et l'administration. 
+ * Version:       1.0.0
+ * Author:        Acary
+ * Author URI:    https://acary.ca
+ * Text Domain:   acary-cloud-helper
+ * Domain Path:   /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
