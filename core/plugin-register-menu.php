a399e67059aab39f0711bf5a84a612de6f2e6f55
HyKo <happy.plan7983@rt.hyko.dev>
2024-11-05 21:09:38 -0500

    Update plugin-register-menu.php

--- core/plugin-register-menu.php
+++ core/plugin-register-menu.php
@@ -11,3 +11,13 @@ add_action('admin_menu', function () {
         'acary_cloud_helper_sendmail_render_settings_page'
     );
 });
+
+add_action('admin_menu', function () {
+    add_options_page(
+        __('Cache', 'acary-cloud-helper'),
+        __('Cache', 'acary-cloud-helper'),
+        'manage_options',
+        'acary_cloud_helper_cache',
+        'acary_cloud_helper_cache_render_settings_page'
+    );
+});
