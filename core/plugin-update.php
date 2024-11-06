f620f7fc52a55bd9545e957e24eda9a0269dbfd2
HyKo <happy.plan7983@rt.hyko.dev>
2024-11-05 21:19:31 -0500

    Update plugin-update.php

--- core/plugin-update.php
+++ core/plugin-update.php
@@ -5,6 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 require_once( ACARYCLOUDHELPER_ABSPATH . '/includes/plugin-update-checker/plugin-update-checker.php' );
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+use YahnisElsts\PluginUpdateChecker\v5p5\Vcs\Api;
 
 $AcaryCloudHelperUpdateChecker = PucFactory::buildUpdateChecker(
     'https://github.com/acaryca/wp-pl-cloud-helper',
@@ -16,5 +17,13 @@ $AcaryCloudHelperUpdateChecker->setBranch('main');
 $AcaryCloudHelperUpdateChecker->getVcsApi()->enableReleaseAssets();
 
 if ( defined('ACARYCLOUDHELPER_VERSIONDEV') && ACARYCLOUDHELPER_VERSIONDEV === true ) {
-    $AcaryCloudHelperUpdateChecker->getVcsApi()->setReleaseFilter('include-prereleases');
+    // Utiliser un filtre personnalisé pour inclure uniquement les pré-releases
+    $AcaryCloudHelperUpdateChecker->getVcsApi()->setReleaseFilter(
+        function($version, $release) {
+            // Vérifie si la version est une pré-release en utilisant une convention de nommage ou une propriété spécifique
+            return (strpos($version, 'beta') !== false || strpos($version, 'alpha') !== false || strpos($version, 'RC') !== false);
+        },
+        Api::RELEASE_FILTER_ALL, // Utiliser cette constante pour inclure tous les types et filtrer manuellement
+        5 // Nombre maximum de releases à examiner
+    );
 }
