0fba1ea3f1112680137d2da1736b37ecad596634
HyKo <happy.plan7983@rt.hyko.dev>
2024-11-06 09:38:39 -0500

    Cache

--- acary-cloud-helper.php
+++ acary-cloud-helper.php
@@ -26,7 +26,9 @@ require_once( ACARYCLOUDHELPER_ABSPATH . '/core/plugin-register-settings.php' );
 require_once( ACARYCLOUDHELPER_ABSPATH . '/core/plugin-register-menu.php' );
 
 require_once( ACARYCLOUDHELPER_ABSPATH . '/admin/sendmail.php' );
-require_once( ACARYCLOUDHELPER_ABSPATH . '/admin/cache.php' );
+if (file_exists(sprintf('%s/.varnish-cache/settings.json', rtrim(getenv('HOME'), '/'))) && is_user_logged_in()) {
+    require_once( ACARYCLOUDHELPER_ABSPATH . '/admin/cache.php' );
+}
 
 add_action('admin_enqueue_scripts', function() {
     if(is_user_logged_in() && is_admin_bar_showing()) {
--- core/plugin-register-menu.php
+++ core/plugin-register-menu.php
@@ -12,43 +12,45 @@ add_action('admin_menu', function () {
     );
 });
 
-add_action('admin_menu', function () {
-    add_options_page(
-        __('Cache', 'acary-cloud-helper'),
-        __('Cache', 'acary-cloud-helper'),
-        'manage_options',
-        'acary_cloud_helper_cache',
-        'acary_cloud_helper_cache_render_settings_page'
-    );
-});
-add_action('admin_bar_menu', function ($adminbar) {
-    if( !is_admin() ) return;
-    if( !current_user_can('manage_options') ) return;
-    
-    $admin_bar_nodes = [
-        [
-            'id'     => 'acary-cloud-helper',
-            'title'  => __('Cache', 'acary-cloud-helper'),
-            'meta'   => ['class' => 'acary-cloud-helper'],
-        ],
-        [
-            'parent' => 'acary-cloud-helper',
-            'id'     => 'acary-cloud-helper-purge',
-            'title'  => __('Purge all', 'acary-cloud-helper'),
-            'href'   => wp_nonce_url(add_query_arg('acary-cloud-helper-cache', 'purge-entire-cache'), 'purge-entire-cache'),
-            'meta'   => [
-                'title' => __( 'Purge all', 'acary-cloud-helper' ),
+if (file_exists(sprintf('%s/.varnish-cache/settings.json', rtrim(getenv('HOME'), '/')))) {
+    add_action('admin_menu', function () {
+        add_options_page(
+            __('Cache', 'acary-cloud-helper'),
+            __('Cache', 'acary-cloud-helper'),
+            'manage_options',
+            'acary_cloud_helper_cache',
+            'acary_cloud_helper_cache_render_settings_page'
+        );
+    });
+    add_action('admin_bar_menu', function ($adminbar) {
+        if( !is_admin() ) return;
+        if( !current_user_can('manage_options') ) return;
+        
+        $admin_bar_nodes = [
+            [
+                'id'     => 'acary-cloud-helper',
+                'title'  => __('Cache', 'acary-cloud-helper'),
+                'meta'   => ['class' => 'acary-cloud-helper'],
             ],
-        ],
-        [
-            'parent' => 'acary-cloud-helper',
-            'id'     => 'acary-cloud-helper-settings',
-            'title'  => __('Settings', 'acary-cloud-helper'),
-            'href'   => admin_url('options-general.php?page=acary_cloud_helper_cache'),
-            'meta'   => [ 'tabindex' => '0' ],
-        ],
-    ];
-    foreach ($admin_bar_nodes as $node) {
-        $adminbar->add_node($node);
-    }
-}, 100);
+            [
+                'parent' => 'acary-cloud-helper',
+                'id'     => 'acary-cloud-helper-purge',
+                'title'  => __('Purge all', 'acary-cloud-helper'),
+                'href'   => wp_nonce_url(add_query_arg('acary-cloud-helper-cache', 'purge-entire-cache'), 'purge-entire-cache'),
+                'meta'   => [
+                    'title' => __( 'Purge all', 'acary-cloud-helper' ),
+                ],
+            ],
+            [
+                'parent' => 'acary-cloud-helper',
+                'id'     => 'acary-cloud-helper-settings',
+                'title'  => __('Settings', 'acary-cloud-helper'),
+                'href'   => admin_url('options-general.php?page=acary_cloud_helper_cache'),
+                'meta'   => [ 'tabindex' => '0' ],
+            ],
+        ];
+        foreach ($admin_bar_nodes as $node) {
+            $adminbar->add_node($node);
+        }
+    }, 100);
+}
